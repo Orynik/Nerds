22903f155e8c37704d3cfcb940c584f991aab86f
Дмитрий <Dkrovel@gmail.com>
2019-09-07 00:30:32 +0800

    Added support IE11+
    *Added check for IE11+
    *Added property "top" for .nav-backet::before
    *Deleted :root (conflicted with IE11+)

--- JS/Script.js
+++ JS/Script.js
@@ -39,6 +39,13 @@ function clearSelect(){
 
 
 //Модальное окно
+// Проверка, является ли браузер IE
+let ChechedBrowser = function(){
+    if(navigator.userAgent.search(/Trident/) > 0){
+        return 2;
+    }
+    return 1;
+}
 
 let modal_call = document.querySelector(".modal-call");
 let close_modal = document.querySelector(".close-cross");
@@ -49,13 +56,21 @@ modal_call.onclick = function(){
     modal_window.style.transition = "all 1s ease";
     document.body.style.overflow = "hidden";
     blur.style.transition = "all 1s ease";
-    blur.style.filter = "blur(3px)"
+    if(ChechedBrowser = 2){
+        blur.style.opacity = "0.2";
+    }else{
+        blur.style.filter = "blur(3px)";
+    }
     modal_window.style.top = "0";
 }
 
 close_modal.onclick = function(){
     modal_window.style.top = "2600px";
-    blur.style.filter = "";
+    if(ChechedBrowser = 2){
+        blur.style.opacity = "1";
+    }else{
+        blur.style.filter = "";
+    }
     document.body.style.overflow = "scroll";
     modal_window.style.transition = "all 2s ease";
 }
